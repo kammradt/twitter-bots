58075392c1771baa48d9846fce277067a23e052b
Vinicius Kammradt <vinicius.kammradt1@gmail.com>
2019-12-25 22:56:02 -0300

    :fire:

--- src/main/java/com/kammradt/twitter/TwitterApplication.java
+++ src/main/java/com/kammradt/twitter/TwitterApplication.java
@@ -3,7 +3,6 @@ package com.kammradt.twitter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import twitter4j.TwitterException;
 
 @SpringBootApplication
 @EnableScheduling
