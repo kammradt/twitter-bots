f14d0b0375cd3bdd3e2ae85fd0fe40ced244c222
Vinicius Kammradt <vinicius.kammradt1@gmail.com>
2019-12-26 02:23:45 -0300

    :art: Better generic parameters

--- src/main/java/com/kammradt/twitter/utils/RandomUtils.java
+++ src/main/java/com/kammradt/twitter/utils/RandomUtils.java
@@ -4,9 +4,9 @@ import java.util.Random;
 
 public class RandomUtils {
 
-    public static int getRandomNumber(Integer range) {
+    public static int getRandomNumber(Number range) {
         Random randomGenerator = new Random();
-        return randomGenerator.nextInt(range);
+        return randomGenerator.nextInt(range.intValue());
     }
 
 }
