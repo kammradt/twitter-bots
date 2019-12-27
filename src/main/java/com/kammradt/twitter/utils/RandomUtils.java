a145506d8fc0973428ea4ba1a6b30bc25c18c6da
Vinicius Kammradt <vinicius.kammradt1@gmail.com>
2019-12-26 23:11:27 -0300

    :sparkles: Adding a generic method to get a random emoji

--- src/main/java/com/kammradt/twitter/utils/RandomUtils.java
+++ src/main/java/com/kammradt/twitter/utils/RandomUtils.java
@@ -1,5 +1,6 @@
 package com.kammradt.twitter.utils;
 
+import java.util.List;
 import java.util.Random;
 
 public class RandomUtils {
@@ -7,6 +8,10 @@ public class RandomUtils {
     public static int getRandomNumber(Number range) {
         Random randomGenerator = new Random();
         return randomGenerator.nextInt(range.intValue());
+    }
+
+    public static String getRandomEmoji(List<String> emojiOptions) {
+        return emojiOptions.get(getRandomNumber(emojiOptions.size()));
     }
 
 }
