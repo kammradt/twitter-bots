5b2da17d7e7d800367706029f6ec6c4e220aa244
Vinicius Kammradt <vinicius.kammradt1@gmail.com>
2019-12-30 23:16:59 -0300

    :card_file_box: Adding MeanGirl Entity

---
+++ src/main/java/com/kammradt/twitter/domain/girls/MeanGirl.java
@@ -0,0 +1,23 @@
+package com.kammradt.twitter.domain.girls;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+public class MeanGirl implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String imageUrl;
+}
